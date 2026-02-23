3f8bb181ddd77a63116894e9840b7a9d68494993
mateuhenriqueoli764-cpu <mateuhenriqueoli764@gmail.com>
2026-02-23 20:07:46 -0300

    https://github.com/mateuhenriqueoli764-cpu/calculadora.git

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function calcular(operacao) {
+const num1 = parseFloat(document.getElementById("num1").value);
+const num2 = parseFloat(document.getElementById("num2").value);
+let resultado;
+switch (operacao) {
+case "somar":
+resultado = num1+num2;
+break;
+case "subtrair":
+resultado = num1-num2;
+break;
+case "multiplicar":
+resultado = num1 * num2;
+break;
+case "dividir":
+resultado = num2 !== 0 ? num1 / num2 : "Erro: divisão por zero";
+break;
+default:
+resultado = "Operação inválida";
+}
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
